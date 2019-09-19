592bcb8c11593d5c165c60b6d019bb4de60e8d07
MehranButt101 <mehranbutt97@gmail.com>
2019-09-19 21:51:23 +0500

    Updates

---
+++ precache-manifest.047b0f54436de7623d56f0bd4ed37625.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4a3eedd64e7498d36766ee1415a1a07e",
+    "url": "/contactmanager/index.html"
+  },
+  {
+    "revision": "60ff58663ca9d37acb04",
+    "url": "/contactmanager/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "47747b035973d0844577",
+    "url": "/contactmanager/static/css/main.cfda8877.chunk.css"
+  },
+  {
+    "revision": "60ff58663ca9d37acb04",
+    "url": "/contactmanager/static/js/2.0fbaba9b.chunk.js"
+  },
+  {
+    "revision": "47747b035973d0844577",
+    "url": "/contactmanager/static/js/main.cede1adc.chunk.js"
+  },
+  {
+    "revision": "b6597f31777016aa759a",
+    "url": "/contactmanager/static/js/runtime~main.77e22976.js"
+  }
+]);
